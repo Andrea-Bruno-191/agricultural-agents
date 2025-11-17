e186392775720434cbaed3cb1cbbac20fe4bb633
Andrea Bruno <bruno.andrea.191@gmail.com>
2025-11-16 18:27:32 -0700

    updated abm!

--- abm-labor-shortage/farm_labor_model_andrea.py
+++ abm-labor-shortage/farm_labor_model_andrea.py
@@ -1,3 +1,29 @@
+#!/usr/bin/python3
+
+import numpy as np
+
 from mesa import Model 
 from mesa.datacollection import DataCollector
 from mesa.discrete_space import OrthogonalMooreGrid 
+from mesa.experimental.continuous_space import ContinuousSpaceAgent 
+
+
+from worker_agent_andrea import current_month, current_year
+
+
+class agricultural_model(Model):
+    def __init__(
+        self, 
+        width = 40,
+        height = 40,
+        worker_density = 0.7, 
+        ICE_density = 0.5
+    ):
+        print('agricultural model init')
+    def step(self):
+        current_month = current_month + 1
+        if current_month > 12:
+            current_month = 1
+            current_year += 1 
+        print(current_year, current_month)
+
--- abm-labor-shortage/worker_agent_andrea.py
+++ abm-labor-shortage/worker_agent_andrea.py
@@ -3,45 +3,61 @@ from enum import Enum
 import mesa
 from mesa.discrete_space import CellAgent
 
+##current month is the global date, all agents are put in 
+current_month = 1
+current_year = 2022
+BOGUS = -1
+
 class WorKerStatus(Enum):
-    """An immigrant farm worker who seeks high-wage employment,
-    subject to concerns about immigration crackdown."""
+    """An immigrant farm worker who will come to the US
+    but leave if wages are too low to compensate for
+    concerns about immigration crackdown."""
+    
     DOCUMTENTED = 1
     UNDOCUMENTED = 2
     DEPORTED = 3
 
-
-
 class Worker(mesa.discrete_space.CellAgent):
-    def 
+    def __init__(self, model, fear, wage_threshold, 
+    wage_constant): 
+        """Create a new worker."""
+        super().__init__(model)
+        self.work_needed = BOGUS##function of the time of year, labor demand 
+                            ##increases in summer and fall
+        self.fear = 1 - math.exp(
+            -1 * round(DEPORTED_COUNT / UNDOCUMENTED_COUNT))
+        self.wage = work_needed / (DOCUMENTED_COUNT + UNDOCUMENTED_COUNT)
+    ##function of work needed vs workers available. The wage offered.
+        self.wage_threshold = self.wage_constant * (1 + self.fear)
 
+        month = 0 
+        work_needed = BOGUS
+     
 
-def __init__(self, model, fear, wage, wage_threshold): 
-    """Create a new worker."""
-    super().__init__(model)
-    self.cell = cell 
-    self.fear = ##(random number between 0 and 1)*number of deported * documented status
-    self.wage = ##function of work needed vs workers available 
-    self.wage_threshold = ##function of fear, hazard dynamic
+    def step(self):
+        self.move()
+        if self.wage_threshold >= self.wage: ##self deport 
+            BOGUS
+        def update_fear(self):
+            print('I should update Fear') 
 
-def step(self):
-    self.move()
-    if self.wage_threshold >= self.wage ##self deport  
+        def update_wage_threshold(self):
+            print('I should update wages')
+        
 
-class ICE_officer(CellAgent)
-def __init__(self, model, vision, deportation):
-super().__init__(model)
-self.vision = vision 
-self.deportation = deportation
+class ICE_officer(mesa.discrete_space.CellAgent):
+    def __init__(self, model, vision, deportation):
+        super().__init__(model)
+        self.vision = BOGUS
+        self.deportation = BOGUS
 
-def step(self):
-    self.update_neighbors()
-    active_neighbors = []
-    for agent in self.neighbors: 
-        if isinstance(agent, Worker) and Worker.state = WorKerStatus.UNDOCUMENTED: ##Wait. Documentation status doesn't change, so maybe alter this?    
+    def step(self):
+        self.update_neighbors()
+        active_neighbors = []
+        for agent in self.neighbors: 
+            if isinstance(agent, Worker) and agent.state == WorKerStatus.UNDOCUMENTED: 
                     undocumented_neighbors.append(agent)
-        if undocumented_neighbors: 
-            detainee = self.random.choice(undocumented_neighbors)
-            detainee.state = WorkerStatus.DEPORTED
-
-    self.move()
+            if undocumented_neighbors: 
+                detainee = self.random.choice(undocumented_neighbors)
+                detainee.state = WorkerStatus.DEPORTED
+        self.move()
