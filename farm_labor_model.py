ca7e1ee816be012c6c539495f5ee0c4e54111225
Anrbruno <bruno.andrea.191@gmail.com>
2025-11-19 21:07:11 -0700

    renaming, general cleanup.

--- farm_labor_model.py
+++ farm_labor_model.py
@@ -8,20 +8,8 @@ from mesa.datacollection import DataCollector
 from mesa.discrete_space import OrthogonalMooreGrid
 from mesa.experimental.continuous_space import ContinuousSpaceAgent
 
-#from worker_agent_andrea import current_month, current_year, WorkerStatus, Worker, ICE_officer
-import worker_agent_andrea
-
-agri_month2work_needed = {1: 27, 2: 33, 3: 33, 4: 33, 5: 33, 6: 33, 7: 33, 8: 33,
-                          9: 33, 10: 40, 11: 41, 12: 33}
-wage_baseline = 17
-
-def calc_wage(year, month, zip_code, n_workers_avail, wage_baseline, work_needed):
-    """Simplified model of wage, uses the agri_month2work_needed table."""
-    work_needed = agri_month2work_needed[month]
-    wage = wage_baseline * work_needed / n_workers_avail 
-    """how many workers we have? should change the work_needed table to be close ##to number of worker agents typically in model"""
-    return wage
-
+from worker_agent import current_month, current_year, WorkerStatus, Worker, ICE_officer
+from farm_wage_utils import work_needed, wage_baseline
 
 class agricultural_model(Model):
     def __init__(
@@ -73,7 +61,7 @@ class agricultural_model(Model):
 
             if klass == ICE_officer:
                 new_ICE_officer = ICE_officer(self, 1, 2)
-                #new_ICE_officer = worker_agent_andrea.ICE_officer(self, vision=ICE_vision)
+                new_ICE_officer = worker_agent.ICE_officer(self, vision=ICE_vision)
                 new_ICE_officer.move_to(cell)
             elif klass == Worker:
                 new_worker = Worker(self, 3, 17, 15)
