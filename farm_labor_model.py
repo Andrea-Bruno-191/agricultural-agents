fe2b31117690180a1c9752b3c83e34f9a798440c
Anrbruno <bruno.andrea.191@gmail.com>
2025-11-19 21:11:12 -0700

    restore to working state, improve functions

--- farm_labor_model.py
+++ farm_labor_model.py
@@ -8,8 +8,21 @@ from mesa.datacollection import DataCollector
 from mesa.discrete_space import OrthogonalMooreGrid
 from mesa.experimental.continuous_space import ContinuousSpaceAgent
 
-from worker_agent import current_month, current_year, WorkerStatus, Worker, ICE_officer
-from farm_wage_utils import work_needed, wage_baseline
+from farm_worker_agent import WorkerStatus, Worker, ICE_officer
+
+# import farm_worker_agent
+
+# agri_month2work_needed = {1: 27, 2: 33, 3: 33, 4: 33, 5: 33, 6: 33, 7: 33, 8: 33,
+#                           9: 33, 10: 40, 11: 41, 12: 33}
+# wage_baseline = 17
+
+# def calc_wage(year, month, zip_code, n_workers_avail, wage_baseline, work_needed):
+#     """Simplified model of wage, uses the agri_month2work_needed table."""
+#     work_needed = agri_month2work_needed[month]
+#     wage = wage_baseline * work_needed / n_workers_avail 
+#     """how many workers we have? should change the work_needed table to be close ##to number of worker agents typically in model"""
+#     return wage
+
 
 class agricultural_model(Model):
     def __init__(
@@ -24,6 +37,9 @@ class agricultural_model(Model):
         max_iters=1000
     ):
         super().__init__(seed=seed)
+        # current month is the global date, all agents are put in 
+        self.current_month = 1
+        self.current_year = 2022
         self.movement = movement
         self.max_iters = max_iters
 
@@ -51,7 +67,7 @@ class agricultural_model(Model):
 
         for cell in self.grid.all_cells:
             print(f"model_init_iterate_cell: {cell}")
-            # new_ICE_officer = worker_agent_andrea.ICE_officer(self, 1, 2)
+            # new_ICE_officer = farm_worker_agent.ICE_officer(self, 1, 2)
             # new_worker = Worker(self, 3, 17, 15)
             klass = self.random.choices(
                 [ICE_officer, Worker, None],
@@ -61,7 +77,7 @@ class agricultural_model(Model):
 
             if klass == ICE_officer:
                 new_ICE_officer = ICE_officer(self, 1, 2)
-                new_ICE_officer = worker_agent.ICE_officer(self, vision=ICE_vision)
+                #new_ICE_officer = farm_worker_agent.ICE_officer(self, vision=ICE_vision)
                 new_ICE_officer.move_to(cell)
             elif klass == Worker:
                 new_worker = Worker(self, 3, 17, 15)
@@ -89,11 +105,11 @@ class agricultural_model(Model):
         if self.steps > self.max_iters:
             self.running = False
 
-        current_month = current_month + 1
-        if current_month > 12:
-            current_month = 1
-            current_year += 1
-        print(current_year, current_month)
+        self.current_month = self.current_month + 1
+        if self.current_month > 12:
+            self.current_month = 1
+            self.current_year += 1
+        print(self.current_year, self.current_month)
 
     def _update_counts(self):
         """helper function for counting number of workers"""
--- farm_wage_utils.py
+++ farm_wage_utils.py
@@ -6,14 +6,16 @@ season, geographical region, and worker availability.
 """
 
 
-agri_month2wlabor_needed = {1: 27, 2: 33, 3: 33, 4: 33, 5: 33, 6: 33, 7: 33, 8: 33,
+agri_month2work_needed = {1: 27, 2: 33, 3: 33, 4: 33, 5: 33, 6: 33, 7: 33, 8: 33,
                           9: 33, 10: 40, 11: 41, 12: 33}
 wage_baseline = 17
 
-def calc_wage(year, month, zip_code, n_workers_avail, wage_baseline, work_needed):
+def calc_wage(year, month, zip_code, n_workers_avail, wage_baseline):
     """Simplified model of wage, uses the agri_month2work_needed table."""
-    labor_needed = agri_month2work_needed[month]
-    wage = wage_baseline * labor_needed / n_workers_avail 
-    """how many workers do we have? should change the work_needed table to be close ##to number of worker agents typically in model"""
+    # work_needed = agri_month2work_needed[month]
+    wage = wage_baseline * get_employment_capacity(year, month) / n_workers_avail 
+    """how many workers we have? should change the work_needed table to be close ##to number of worker agents typically in model"""
     return wage
 
+def get_employment_capacity(year, month):
+    return agri_month2work_needed[month]
